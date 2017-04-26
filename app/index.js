c67769f8d06df47349ba35b4114ea41050354e9d
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-26 12:05:33 +0800

    Add basic fs reading functionality

--- app/index.js
+++ app/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const commander = require('commander');
+const program = require('commander');
 const packageJson = require('../package.json');
-const program = commander.Command(packageJson.name)
+const fs = require('fs');
+const config = '.tmux-grid.yml';
+const encoding = 'utf8';
 
 program
   .version(packageJson.version)
-  .on('--help', () => {
-    console.log('Run tmux-grid inside a project directory with a .tmux-grid configuration file'):
-  })
+  .option('-c, --config [value]', 'specify alternate configuration file (default: .tmux-grid.yml)')
+  .parse(process.argv);
+  
+if (program.config === true) return program.outputHelp();
+  
+fs.readFile(program.config || config, encoding, (err, data) => {
+  if (err) return console.error(err.message);
+  
+  // TODO: app logic here
+  // TODO: error handling for unknown commands
+  // TODO: better help output
+  // console.log(data);
+});

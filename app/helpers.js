6eef58516c130934300c876669a87d99f6f43aac
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-07-07 16:22:10 -0700

    Add config validation step

--- app/helpers.js
+++ app/helpers.js
@@ -1,21 +1,23 @@
 const jsyaml = require('js-yaml');
 
 function translateConfig(configFile) {
-  let config;
+  let config, error;
 
   try {
     config = jsyaml.load(configFile);
   } catch(e) {
-    return console.error(e);
+    console.error(e);
   }
 
-  // TODO: error handling for unknown commands
+  if (!config || config === '') return new Error('Invalid configuration');
+
   const commands = [];
 
   if (config.panes && config.panes.length % 2 === 0) {
-    console.log(config.panes);
+    // console.log(config.panes);
   }
 
+  // TODO: error handling for unknown commands
   commands.push(config.command);
 
   return commands;
--- app/helpers.test.js
+++ app/helpers.test.js
@@ -2,12 +2,21 @@ const fs = require('fs');
 const { translateConfig } = require('./helpers.js');
 
 describe('translateConfig', () => {
-  const configFile = 'examples/sample2.yml';
+  describe('when passed with a valid config', () => {
+    const configFile = 'examples/sample2.yml';
 
-  it('should return json', () => {
-    fs.readFile(configFile, (err, configFile) => {
-      const commands = translateConfig(configFile);
-      expect(commands).toBe(false);
+    it('should return json', () => {
+      fs.readFile(configFile, (err, config) => {
+        const commands = translateConfig(config);
+        expect(commands).toBe(false);
+      });
+    });
+  });
+
+  describe('when passed with an invalid config', () => {
+    it('should respond with an error', () => {
+      const commands = translateConfig('');
+      expect(commands.message).toBe('Invalid configuration');
     });
   });
 });

7f997af2ce3c5d8635dbde2904760f53458f02d3
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-07-11 16:16:58 -0700

    Add specs for sample3 config file

--- app/helpers.test.js
+++ app/helpers.test.js
@@ -56,6 +56,29 @@ describe('translateConfig', () => {
     });
   });
 
-  // describe('when passed with config examples/sample3.yml');
-  // TODO: error handling for unknown commands
+  describe('when passed with config examples/sample3.yml', () => {
+    const configFile = 'examples/sample3.yml';
+
+    // it('should return appropriate commands', (done) => {
+    //   fs.readFile(configFile, (err, config) => {
+    //     const commands = translateConfig(config);
+    //     expect(Array.isArray(commands)).toBe(true);
+    //     expect(commands[0]).toBe('tmux rename-window sample2');
+    //     expect(commands[1]).toBe('tmux split-window -d');
+    //     expect(commands[2]).toBe('tmux split-window -hd');
+    //     expect(commands[3]).toBe('tmux send-keys "echo something1" Enter');
+    //     expect(commands[4]).toBe('tmux select-pane -R');
+    //     expect(commands[5]).toBe('tmux send-keys "echo something2" Enter');
+    //     expect(commands[6]).toBe('tmux select-pane -R');
+    //     expect(commands[7]).toBe('tmux select-pane -D');
+    //     expect(commands[8]).toBe('tmux split-window -hd');
+    //     expect(commands[9]).toBe('tmux send-keys "echo something3" Enter');
+    //     expect(commands[10]).toBe('tmux select-pane -R');
+    //     expect(commands[11]).toBe('tmux send-keys "echo something4" Enter');
+    //     expect(commands[12]).toBe('tmux select-pane -R');
+    //     expect(commands[13]).toBe('tmux select-pane -D');
+    //     done();
+    //   });
+    // });
+  });
 });
